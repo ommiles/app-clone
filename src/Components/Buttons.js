4a96032f4eef51b843d5d3fd5d822c70712f29c6
OM Miles <miles@ommiles.com>
2021-08-21 19:58:51 -0700

    buttons added and styled

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import ReplayIcon from '@material-ui/icons/Replay';
+import CloseIcon from '@material-ui/icons/Close';
+import StarRateIcon from '@material-ui/icons/StarRate';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FlashOnIcon from '@material-ui/icons/FlashOn';
+import IconButton from '@material-ui/core/IconButton';
 
 export const Buttons = () => {
   return (
-    <div>
-      <h1>Buttons</h1>
+    <div className='buttons'>
+      <IconButton className='buttons__repeat' >
+        <ReplayIcon fontSize='large' />
+      </IconButton>
+      <IconButton className='buttons__left' >
+        <CloseIcon fontSize='large' />
+      </IconButton>
+      <IconButton className='buttons__star' >
+        <StarRateIcon fontSize='large' />
+      </IconButton>
+      <IconButton className='buttons__favorite' >
+        <FavoriteIcon fontSize='large' />
+      </IconButton>
+      <IconButton className='buttons__lightning' >
+        <FlashOnIcon fontSize='large' />
+      </IconButton>
     </div>
   );
 };
